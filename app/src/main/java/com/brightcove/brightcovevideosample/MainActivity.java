e80faa1a1411cb34d877aac6c142f78a8106cc86
Sergio Martinez <smartinez@brightcove.com>
2017-02-23 10:03:10 -0500

    Added Brightcove Video view programatically

--- app/src/main/java/com/brightcove/brightcovevideosample/MainActivity.java
+++ app/src/main/java/com/brightcove/brightcovevideosample/MainActivity.java
@@ -1,21 +1,30 @@
 package com.brightcove.brightcovevideosample;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 import com.brightcove.player.edge.Catalog;
 import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.Component;
+import com.brightcove.player.event.Default;
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.event.EventListener;
 import com.brightcove.player.event.EventType;
+import com.brightcove.player.event.ListensFor;
 import com.brightcove.player.mediacontroller.BrightcoveMediaController;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.util.LifecycleUtil;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.brightcove.player.view.BrightcovePlayer;
 import com.brightcove.player.view.BrightcoveVideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     private LifecycleUtil lifecycleUtil;
     private BrightcoveVideoView brightcoveVideoView;
@@ -24,10 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
-        brightcoveVideoView.setMediaController((BrightcoveMediaController) null);
 
-
+        initVideoView();
         if (lifecycleUtil == null || lifecycleUtil.baseVideoView != brightcoveVideoView) {
             lifecycleUtil = new LifecycleUtil(brightcoveVideoView);
             lifecycleUtil.onCreate(savedInstanceState, this);
@@ -46,6 +53,56 @@ public class MainActivity extends AppCompatActivity {
                 brightcoveVideoView.start();
             }
         });
+
+        initListeners(eventEmitter);
+    }
+
+    private void initVideoView() {
+        if (brightcoveVideoView == null) {
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            brightcoveVideoView = new BrightcoveExoPlayerVideoView(this);
+            brightcoveVideoView.setMediaController((BrightcoveMediaController) null);
+            brightcoveVideoView.finishInitialization();
+
+            linearLayout.addView(brightcoveVideoView, layoutParams);
+        }
+    }
+
+    private void initListeners(@NonNull EventEmitter eventEmitter) {
+        eventEmitter.on(EventType.PROGRESS, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                Log.v(TAG, "Reporting progress: "+ event.getIntegerProperty(Event.PLAYHEAD_POSITION));
+            }
+        });
+
+        EventListener myListener = new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                Log.i(TAG, "EventType:"+event.getType());
+                switch(event.getType()) {
+                    case EventType.PLAY:
+                        break;
+                    case EventType.DID_PLAY:
+                        break;
+                    case EventType.PAUSE:
+                        break;
+                    case EventType.DID_PAUSE:
+                        break;
+                }
+            }
+        };
+        //Adding events to the event emitter
+        eventEmitter.on(EventType.PLAY, myListener);
+        eventEmitter.on(EventType.DID_PLAY, myListener);
+        eventEmitter.on(EventType.PAUSE, myListener);
+        eventEmitter.on(EventType.DID_PAUSE, myListener);
+
     }
 
     @Override
@@ -82,5 +139,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         lifecycleUtil.activityOnStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle bundle) {
+        // Give all the listeners a chance to run before calling super.
+        brightcoveVideoView.getEventEmitter().on(EventType.ACTIVITY_SAVE_INSTANCE_STATE, new EventListener() {
+            @Override
+            @Default
+            public void processEvent(Event event) {
+                MainActivity.super.onSaveInstanceState(bundle);
+            }
+        });
+
+        lifecycleUtil.activityOnSaveInstanceState(bundle);
     }
 }
