256d8573fa502ce6137edeff044c15c2a19b8c1b
Sergio Martinez <smartinez@brightcove.com>
2017-02-23 12:50:42 -0500

    Added full screen support when in landscape mode

--- app/src/main/java/com/brightcove/brightcovevideosample/MainActivity.java
+++ app/src/main/java/com/brightcove/brightcovevideosample/MainActivity.java
@@ -2,39 +2,32 @@ package com.brightcove.brightcovevideosample;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.res.Configuration;
 import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.StateListDrawable;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.brightcove.player.edge.Catalog;
 import com.brightcove.player.edge.VideoListener;
-import com.brightcove.player.event.Component;
 import com.brightcove.player.event.Default;
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.event.EventListener;
 import com.brightcove.player.event.EventType;
-import com.brightcove.player.event.ListensFor;
 import com.brightcove.player.mediacontroller.BrightcoveMediaController;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.util.LifecycleUtil;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
-import com.brightcove.player.view.BrightcovePlayer;
 import com.brightcove.player.view.BrightcoveVideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -50,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         initVideoView();
-        if (lifecycleUtil == null || lifecycleUtil.baseVideoView != brightcoveVideoView) {
+        if (brightcoveVideoView != null &&
+                (lifecycleUtil == null || lifecycleUtil.baseVideoView != brightcoveVideoView)) {
             lifecycleUtil = new LifecycleUtil(brightcoveVideoView);
             lifecycleUtil.onCreate(savedInstanceState, this);
         }
@@ -72,6 +66,23 @@ public class MainActivity extends AppCompatActivity {
         initListeners(eventEmitter);
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        lifecycleUtil.onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            emitEvent(EventType.ENTER_FULL_SCREEN);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            emitEvent(EventType.EXIT_FULL_SCREEN);
+        }
+    }
+
+    private void emitEvent(String eventType) {
+        if (brightcoveVideoView != null) {
+            brightcoveVideoView.getEventEmitter().emit(eventType);
+        }
+    }
+
     private void initVideoView() {
         if (brightcoveVideoView == null) {
             LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
@@ -91,13 +102,10 @@ public class MainActivity extends AppCompatActivity {
 
     private int getVideoHeight() {
         Point size = getDisplaySize();
-        int width;
         int height;
         if (size.x > size.y) {
             height = size.x;
-            width = size.y;
         } else {
-            width = size.x;
             height = size.y;
         }
 
@@ -119,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
         return point;
     }
 
+    //Creating one button to handle Play and Pause
     private LinearLayout createController() {
         LinearLayout parent = new LinearLayout(this);
 
@@ -158,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListeners(@NonNull EventEmitter eventEmitter) {
+        //Adding one event to a Event Listener
         eventEmitter.on(EventType.PROGRESS, new EventListener() {
             @Override
             public void processEvent(Event event) {
@@ -172,19 +182,31 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "EventType:"+event.getType());
                 switch(event.getType()) {
                     case EventType.PLAY:
-                        break;
                     case EventType.DID_PLAY:
-                        break;
                     case EventType.PAUSE:
-                        break;
                     case EventType.DID_PAUSE:
+                        //Do something.
+                        break;
+                    case EventType.VIDEO_SIZE_KNOWN:
+                        int height = event.getIntegerProperty(Event.VIDEO_HEIGHT);
+                        int width = event.getIntegerProperty(Event.VIDEO_WIDTH);
+                        Log.i(TAG, "Video height:"+height +", width:"+width);
+                        break;
+                    case EventType.VIDEO_DURATION_CHANGED:
+                        int duration = event.getIntegerProperty(Event.VIDEO_DURATION);
+                        Log.i(TAG, "Video duration:"+duration);
+                        break;
+                    case EventType.DID_SET_VIDEO:
+                        Video video = (Video) event.properties.get(Event.VIDEO);
+
+                        Log.i(TAG, "Video set - :"+video.toString());
                         break;
                     default:
                         break;
                 }
             }
         };
-        //Adding events to the event emitter
+        //Adding several events to the same Event Listener.
         eventEmitter.on(EventType.PLAY, myListener);
         eventEmitter.on(EventType.DID_PLAY, myListener);
         eventEmitter.on(EventType.PAUSE, myListener);
@@ -195,6 +217,9 @@ public class MainActivity extends AppCompatActivity {
         eventEmitter.on(EventType.DID_SET_VIDEO, myListener);
 
     }
+
+
+    //Setting LifeCycleUtil calls
 
     @Override
     protected void onStart() {
